470e0048c6266c05261f28488e8abb29d2a858e4
Krzysztof Zduniak <krzysz.zdun@gmail.com>
2021-08-26 17:27:40 +0200

    book records are now added from script

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ function Book(title, author, pages, haveRead) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.haveRead = haveRead;
+    this.readStatus = haveRead;
 }
 
 Book.prototype.info = function () {
-    if (this.haveRead) {
+    if (this.readStatus) {
         return `${this.title} by ${this.author}, ${this.pages}, has been read already`;
     } else {
         return `${this.title} by ${this.author}, ${this.pages}, has been read already`;
@@ -14,7 +14,7 @@ Book.prototype.info = function () {
 };
 
 Book.prototype.changeReadStatus = function () {
-    this.haveRead = !this.haveRead;
+    this.readStatus = !this.readStatus;
 };
 
 let myLibrary = [];
@@ -23,51 +23,76 @@ function addBookToLibrary(title, author, pages, haveRead) {
     myLibrary.push(new Book(title, author, pages, haveRead));
 }
 
+addBookToLibrary("asgfa", "ds", 200, true);
+
+const table = document.querySelector(".bookTable");
+table.removeChild(table.firstChild);
 function updateLibrary() {
-    const table = document.querySelector(".bookTable");
     // while (table.firstChild) {
+    //     if (table.firstChild.classList.contains("addBook")) break;
     //     table.removeChild(table.firstChild);
     // }
 
+    // creating a doc element for each book in library
     myLibrary.forEach((book, index) => {
-        const row = document.createElement("div");
-        row.classList.add("row");
-        row.dataset.index = index;
-        Object.keys(book).forEach((key) => {
-            const cell = document.createElement("div");
-            cell.classList.add(`${key}`, "cell");
-            cell.textContent = book[key];
-            row.appendChild(cell);
-        });
-        const markRead = document.createElement("button");
-        markRead.classList.add("cell");
-        markRead.style.height = "22px";
-        markRead.textContent = "./";
-        markRead.style.padding = "0px 4px 2px";
-        markRead.style.float = "right";
-        markRead.addEventListener("click", () => {
-            book.changeReadStatus();
-            updateLibrary();
-        });
+        table.removeChild(table.firstChild);
+        const record = document.createElement("div");
+        record.classList.add("record");
 
-        row.querySelector(".haveRead").appendChild(markRead);
+        //
+        const bookDetails = document.createElement("span");
+        const title = document.createElement("h3");
+        title.classList.add("title");
+        title.textContent = book["title"];
+        bookDetails.appendChild(title);
 
-        const deleteRecord = document.createElement("button");
-        deleteRecord.dataset.index = index;
-        deleteRecord.classList.add("cell");
-        deleteRecord.textContent = "X";
-        deleteRecord.style.height = "34px";
-        deleteRecord.addEventListener("click", () => {
-            myLibrary.splice(deleteRecord.dataset.index, 1);
-            updateLibrary();
-        });
-        row.appendChild(deleteRecord);
-        table.appendChild(row);
+        const author = document.createElement("span");
+        author.classList.add("author");
+        author.textContent = `by ${book["author"]}`;
+        bookDetails.appendChild(author);
+        const br1 = document.createElement("br");
+        bookDetails.appendChild(br1);
+
+        const pages = document.createElement("span");
+        pages.classList.add("pages");
+        pages.textContent = `${book["pages"]} pages`;
+        bookDetails.appendChild(pages);
+        const br2 = document.createElement("br");
+        bookDetails.appendChild(br2);
+
+        const readStatus = document.createElement("span");
+        readStatus.classList.add("readStatus");
+        if (book["readStatus"]) readStatus.textContent = "read";
+        else readStatus.textContent = "to read";
+        bookDetails.appendChild(readStatus);
+        record.appendChild(bookDetails);
+
+        const buttons = document.createElement("div");
+        buttons.classList.add("buttons");
+
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.classList.add("markRead");
+        if (book.readStatus) checkbox.checked = true;
+
+        buttons.appendChild(checkbox);
+
+        const deleteBook = document.createElement("button");
+        deleteBook.classList.add("delete");
+
+        const img = document.createElement("img");
+        img.src = "1x/outline_close_white_24dp.png";
+        deleteBook.appendChild(img);
+        buttons.appendChild(deleteBook);
+
+        record.appendChild(buttons);
+
+        record.dataset.index = index;
+        table.appendChild(record);
     });
 }
 updateLibrary();
 
-const newBookForm = document.querySelector(".form");
 // library.removeChild(newBookForm);
 
 const addBook = document.querySelector(".addBook");
